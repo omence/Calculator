13e1a383845abac70d9ddc648293eb8e4173f9f4
Jason Few <omence11@gmail.com>
2019-01-07 11:31:34 -0800

    Refactored

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,53 +6,51 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            //creating variables
             int firstNum;
             int secondNum;
             string operation;
             int answer;
 
+            //Name of calculator
             Console.WriteLine("The Best Worst Calculator!");
             Console.ReadLine();
 
+            //request to user for 1st num
             Console.Write("Enter your first number: ");
             firstNum = Convert.ToInt32(Console.ReadLine());
 
+            //request from user for second num
             Console.Write("Enter your second number: ");
             secondNum = Convert.ToInt32(Console.ReadLine());
 
+            //request from user for operator
             Console.Write("Now enter your operator (x, /, +, -)");
             operation = Console.ReadLine();
 
+            //logic for
             if (operation == "x")
             {
 
-                answer = firstNum * secondNum;
-                Console.WriteLine(firstNum + " x " + secondNum + " = " + answer);
-                Console.ReadLine();
+                Multiply(firstNum, secondNum);
             }
 
             else if (operation == "/")
             {
 
-                answer = firstNum / secondNum;
-                Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
-                Console.ReadLine();
+                Divide(firstNum, secondNum);
             }
 
             else if (operation == "+")
             {
 
-                answer = firstNum + secondNum;
-                Console.WriteLine(firstNum + " + " + secondNum + " = " + answer);
-                Console.ReadLine();
+                Add(firstNum, secondNum);
             }
 
             else if (operation == "-")
             {
 
-                answer = firstNum - secondNum;
-                Console.WriteLine(firstNum + " - " + secondNum + " = " + answer);
-                Console.ReadLine();
+                Subtract(firstNum, secondNum);
             }
 
             else {
@@ -63,6 +61,38 @@ namespace Calculator
 
         }
 
-            
+        public static void Multiply(int firstNum, int secondNum) {
+            int answer;
+            answer = firstNum * secondNum;
+            Console.WriteLine(firstNum + " x " + secondNum + " = " + answer);
+            Console.ReadLine();
+        }
+
+        public static void Divide(int firstNum, int secondNum)
+        {
+            int answer;
+            answer = firstNum / secondNum;
+            Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
+            Console.ReadLine();
+        }
+
+        public static void Add(int firstNum, int secondNum)
+        {
+            int answer;
+            answer = firstNum + secondNum;
+            Console.WriteLine(firstNum + " + " + secondNum + " = " + answer);
+            Console.ReadLine();
+        }
+
+        public static void Subtract(int firstNum, int secondNum)
+        {
+            int answer;
+            answer = firstNum / secondNum;
+            Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
+            Console.ReadLine();
+        }
+
+
+
     }
 }
