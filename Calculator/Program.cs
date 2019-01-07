da8ea67e3875da2ac60939dea00c076ceed4d793
Jason Few <omence11@gmail.com>
2019-01-07 10:33:19 -0800

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int secondNum;
+            string operation;
+            int answer;
+
+            Console.WriteLine("The Best Worst Calculator!");
+            Console.ReadLine();
+
+            Console.Write("Enter your first number: ");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your second number: ");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Now enter your operator (x, /, +, -)");
+            operation = Console.ReadLine();
+
+            if (operation == "x")
+            {
+
+                answer = firstNum * secondNum;
+                Console.WriteLine(firstNum + " x " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+
+            else if (operation == "/")
+            {
+
+                answer = firstNum / secondNum;
+                Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+
+            else if (operation == "+")
+            {
+
+                answer = firstNum + secondNum;
+                Console.WriteLine(firstNum + " + " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+
+            else if (operation == "-")
+            {
+
+                answer = firstNum - secondNum;
+                Console.WriteLine(firstNum + " - " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+
+            else {
+                Console.WriteLine("Wrong format, please restart");
+                Console.ReadLine();
+            }
+
+
+        }
+
+            
+    }
+}
